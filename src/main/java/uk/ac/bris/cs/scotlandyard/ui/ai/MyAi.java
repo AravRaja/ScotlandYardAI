9a5d18027c368ebe4ac4f46576870cef957d9bb4
Jack <roddmajb@outlook.com>
2024-04-03 13:08:07 +0100

    detective loactions and adjacent nodes counter

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,22 +1,37 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
+
 import io.atlassian.fugue.Pair;
-import uk.ac.bris.cs.scotlandyard.model.Ai;
-import uk.ac.bris.cs.scotlandyard.model.Board;
-import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 public class MyAi implements Ai {
 
-	@Nonnull @Override public String name() { return "Name me!"; }
+	@Nonnull @Override public String name() { return "AI"; }
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
 		// returns a random move, replace with your own implementation
 		var moves = board.getAvailableMoves().asList();
-		return moves.get(new Random().nextInt(moves.size()));
+		List<Move.SingleMove> singleMoves = new ArrayList<>();
+		List<Move.DoubleMove> doubleMoves = new ArrayList<>();
+		List<Optional<Integer>> detLocs = new ArrayList<>();
+		for (Piece.Detective d : Piece.Detective.values()) {
+			detLocs.add(board.getDetectiveLocation(d));
+		}
+		List<Integer> scores = new ArrayList<>();
+		for (Move m : moves) {
+			if (m instanceof Move.SingleMove) {
+				scores.add(board.getSetup().graph.adjacentNodes(((Move.SingleMove) m).destination).size());
+			}
+			else {
+				scores.add(board.getSetup().graph.adjacentNodes(((Move.DoubleMove) m).destination2).size());
+			}
+		}
+		int maxI = scores.indexOf(Collections.max(scores));
+		return moves.get(maxI);
 	}
 }
